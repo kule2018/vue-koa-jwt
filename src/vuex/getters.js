857d2efe062651286df888f6d7468350fc00da17
Tony <shzym86@163.com>
2019-02-05 21:45:16 +0800

    修复token过期仍保持登录状态的问题

--- src/vuex/getters.js
+++ src/vuex/getters.js
@@ -1,3 +1,5 @@
 import { isEmpty } from 'lodash'
 
-export const isLogged = ({ token }) => !isEmpty(token)
+// 重新打开浏览器，token首先会从本地缓存中恢复，但是如果这个token已经过期，调用API是获取不到的user的
+// 所以即便 setToken 成功了，但是 setUser 失败还是要判定为未登录状态！
+export const isLogged = ({ token, user }) => !isEmpty(token) && !isEmpty(user)
